41bc3cbfefc2a988323abbff13f741798cc406f0
Saigo Ekitae <saigoekitae@gmail.com>
2026-01-30 16:24:49 +0100

    add fish complete script

---
+++ shells/silex.fish
@@ -0,0 +1,32 @@
+complete -c silex -f
+
+complete -c silex -n "__fish_use_subcommand" -a new -d 'Create a new silex project'
+complete -c silex -n "__fish_use_subcommand" -a status -d 'Show changes in working directory'
+complete -c silex -n "__fish_use_subcommand" -a log -d 'Show commit logs'
+complete -c silex -n "__fish_use_subcommand" -a diff -d 'Show changes between working tree and last commit'
+complete -c silex -n "__fish_use_subcommand" -a restore -d 'Discard changes in working directory'
+complete -c silex -n "__fish_use_subcommand" -a commit -d 'Record changes to the repository'
+complete -c silex -n "__fish_use_subcommand" -a sync -d 'Backup repository to a destination'
+complete -c silex -n "__fish_use_subcommand" -a branch -d 'Create a new branch'
+complete -c silex -n "__fish_use_subcommand" -a checkout -d 'Switch branches or restore working tree files'
+complete -c silex -n "__fish_use_subcommand" -a feat -d 'Manage feature branches'
+complete -c silex -n "__fish_use_subcommand" -a hotfix -d 'Manage hotfix branches'
+complete -c silex -n "__fish_use_subcommand" -a tag -d 'Manage version tags'
+complete -c silex -n "__fish_use_subcommand" -a web -d 'Start the web interface'
+
+complete -c silex -n "__fish_seen_subcommand_from commit" -s m -l message -r -d 'Description of the changes'
+
+complete -c silex -n "__fish_seen_subcommand_from restore" -F
+
+complete -c silex -n "__fish_seen_subcommand_from sync" -F
+
+complete -c silex -n "__fish_seen_subcommand_from web" -s p -l port -x -d 'Port (default 3000)'
+
+complete -c silex -n "__fish_seen_subcommand_from feat; and not __fish_seen_subcommand_from start finish" -a "start finish"
+
+complete -c silex -n "__fish_seen_subcommand_from hotfix; and not __fish_seen_subcommand_from start finish" -a "start finish"
+
+complete -c silex -n "__fish_seen_subcommand_from tag; and not __fish_seen_subcommand_from create list" -a "create list"
+complete -c silex -n "__fish_seen_subcommand_from tag; and __fish_seen_subcommand_from create" -s m -l message -r -d 'Description'
+
+complete -c silex -s h -l help -d 'Print a short help text and exit'
